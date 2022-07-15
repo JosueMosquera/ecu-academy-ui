cb4aa2b9b31d1e128925dc023f29153eaf500f87
ecuacademy <ecuacademy.yavirac@gmail.com>
2022-07-15 02:49:56 +0000

    containerAdd

--- src/components/MainWrapper.jsx
+++ src/components/MainWrapper.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const MainWrapper = () => {
   return (
     <>
-    <div style={{marginTop:10}}>
+    <div style={{marginTop:10,display:'grid',gridColumn:'1fr 1fr'}}>
+        <div className='container'>
         Soy el Main Wrapper
-    <span>Login</span>
+        <span>Login</span>
+        </div>
     <img src="https://images.unsplash.com/photo-1546410531-bb4caa6b424d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" style={{width:'50%',height:'50%'}}/>
     </div>
     </>
