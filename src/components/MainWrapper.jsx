bd52984f346658e6f364faecb3f1b48d56cced64
ecuacademy <ecuacademy.yavirac@gmail.com>
2022-07-15 02:55:38 +0000

    justifyContent

--- src/components/MainWrapper.jsx
+++ src/components/MainWrapper.jsx
@@ -8,7 +8,7 @@ const MainWrapper = () => {
         Soy el Main Wrapper
         <span>Login</span>
         </div>
-        <div className='img-container' style={{display:'flex',alignItems:'center'}}>
+        <div className='img-container' style={{display:'flex',alignItems:'center',justifyContent:'center',marginTop:10}}>
         <img src="https://images.unsplash.com/photo-1546410531-bb4caa6b424d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" style={{width:'50%',height:'50%'}}/>
         </div>
     
