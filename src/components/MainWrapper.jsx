5371c1b9e1ff3a5f9922f0ebd60874720986bbeb
alejandroo98 <jeffersonalejandro27@gmail.com>
2022-07-20 19:40:03 -0500

    Primer commit

--- src/components/MainWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MainWrapper = () => {
-  return (
-    <div>Soy el Main Wrapper</div>
-  )
-}
-
-export default MainWrapper
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './AcademyApp';
+
+//Punto de entrada de la app
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
