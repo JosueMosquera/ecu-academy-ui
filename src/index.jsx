ec324a80cea947bf3cbe75de22d25c511408cf1d
ecuacademy <ecuacademy.yavirac@gmail.com>
2022-08-05 05:27:24 +0000

    testJenkins

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './AcademyApp';
 import './css/general.css';
 
-//Punto de entrada de la app
+//Punto de entrada de la app-prueba-jenkis
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 
