792842347d99dce4e95c88694ce0fc819e1c3806
alejandroo98 <jeffersonalejandro27@gmail.com>
2022-08-15 19:54:00 -0500

    videSrean

--- src/ClasesScreen.jsx
+++ src/ClasesScreen.jsx
@@ -28,15 +28,62 @@ const ClasesScreen = () => {
 		},
 	];
 
+	var continuarViendo = [
+		{
+			nombre: 'Js',
+			desc: 'El mejor curso de js',
+			img: 'https//www.google.com',
+		},
+	];
+
 	return (
-		<div className='signupScreen'>
-			<div className='explorar-video'>
-				<h5>Explorar</h5>
-			</div>
-			<div className='video-item-container-grid'>
-				{videos.map(({ nombre, desc, img }) => (
-					<VideoItem nombre={nombre} desc={desc} img={img} />
-				))}
+		<div className='videosScreen'>
+			<div>
+				<div className='columna-video'>
+					<div>
+						<h1>Hola, User</h1>
+					</div>
+				</div>
+				<div className='data-user'>
+					<table>
+						<thead>
+							<tr>
+								<th>TIEMPO DE ESTUDIO</th>
+								<th>CURSOS APROBADOS</th>
+								<th>CURSOS SUCRITOS</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>2 cursos</td>
+								<td>0 curso</td>
+								<td>1 curso</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div className='columna-video'>
+					<div className='title-video'>
+						<h5>Continuar viendo</h5>
+					</div>
+					<div>
+						<div className='video-item-container-grid'>
+							{continuarViendo.map(({ nombre, desc, img }) => (
+								<VideoItem nombre={nombre} desc={desc} img={img} />
+							))}
+						</div>
+					</div>
+				</div>
+				<div className='columna-video'>
+					<div className='title-video'>
+						<h5>Explorar</h5>
+					</div>
+					<div className='video-item-container-grid'>
+						{videos.map(({ nombre, desc, img }) => (
+							<VideoItem nombre={nombre} desc={desc} img={img} />
+						))}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
